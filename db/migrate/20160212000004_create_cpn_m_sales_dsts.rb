88617bee66ece28573761e1cfccca90378fd7943
watanabe@tech-arts.co.jp <watanabe@tech-arts.co.jp>
2016-02-16 17:11:35 +0900

    change layout cpn_ms

--- db/migrate/20160212000004_create_cpn_m_sales_dsts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCpnMSalesDsts < ActiveRecord::Migration
-  def change
-    create_table :cpn_m_sales_dsts, id: false do |t|
-      t.column :CPN_ID, :integer, unsigned: true, :default =>0, :null => false
-      t.column :ACCNT_CD, :string, :limit => 30, :null => false
-    end
-    execute "ALTER TABLE cpn_m_sales_dsts ADD PRIMARY KEY (CPN_ID);"
-  end
-end
