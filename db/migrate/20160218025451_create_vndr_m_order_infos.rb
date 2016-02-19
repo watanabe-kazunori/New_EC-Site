2cb218760a92ebcb442749ca9a1846c2b27b5be6
高部 真一郎 <takabes00@takabe-Macbook-Air.local>
2016-02-19 11:42:58 +0900

    add vnpr

--- db/migrate/20160218025451_create_vndr_m_order_infos.rb
+++ db/migrate/20160218025451_create_vndr_m_order_infos.rb
@@ -2,12 +2,12 @@ class CreateVndrMOrderInfos < ActiveRecord::Migration
   def change
     create_table :vndr_m_order_infos do |t|
      	t.string :accnt_cd, limit: 30, null: false
-		t.string :info_no, limit: 30, null: false
-		t.string :content, limit: 30
-		t.string :disp_odrer, limit: 30
-		t.string :rgstr_usr, limit: 16
-		t.string :lst_upd_usr, limit: 16
-      	t.timestamps null: false
+		  t.string :info_no, limit: 30, null: false
+		  t.string :content, limit: 30
+		  t.string :disp_odrer, limit: 30
+		  t.string :rgstr_usr, limit: 16
+		  t.string :lst_upd_usr, limit: 16
+    	t.timestamps null: false
     end
   end
 end
